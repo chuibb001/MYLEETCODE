63bfc2730e1921cb042ffa7372980c8167de8d87
Simon <44067486@qq.com>
2021-11-25 16:49:22 +0800

    cc
    cc
    
    Co-Authored-By: Simon Lelievre <github@sim.ovh>

--- LEETCODE/LEETCODE/BFile.hpp
+++ LEETCODE/LEETCODE/BFile.hpp
@@ -10,6 +10,8 @@
 
 #include <stdio.h>
 #include <vector>
+#include <string>
+#include <stack>
 
 using namespace std;
 
@@ -169,6 +171,45 @@ public:
         ++p0;
       }
     }
+  };
+  
+//  给定一个只包含三种字符的字符串：（ ，） 和 *，写一个函数来检验这个字符串是否为有效字符串。有效字符串具有如下规则：
+//
+//  任何左括号 ( 必须有相应的右括号 )。
+//  任何右括号 ) 必须有相应的左括号 ( 。
+//  左括号 ( 必须在对应的右括号之前 )。
+//  * 可以被视为单个右括号 ) ，或单个左括号 ( ，或一个空字符串。
+//  一个空字符串也被视为有效字符串。
+  bool checkValidString(string s) {
+    stack<int> left_stack;
+    stack<int> star_stack;
+    for (int i = 0; i < s.length(); i ++) {
+      char c = s[i];
+      if (c == '(') {
+        left_stack.push(i);
+      } else if (c == '*') {
+        star_stack.push(i);
+      } else if (c == ')') {
+        if (!left_stack.empty()) {
+          left_stack.pop();
+        } else if (!star_stack.empty()) {
+          star_stack.pop();
+        } else {
+          return false;
+        }
+      }
+    } // end for
+    while (!left_stack.empty() && !star_stack.empty()) {
+      int left_index = left_stack.top();
+      left_stack.pop();
+      int star_index = star_stack.top();
+      star_stack.pop();
+      if (left_index > star_index) {
+        return false;
+      }
+    }
+    
+    return left_stack.empty();
   };
 };
 
