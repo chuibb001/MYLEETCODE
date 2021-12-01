0aad635746ace93853358e4262745357956caf8d
Simon <simonyuyan@tencent.com>
2021-12-01 21:01:04 +0800

    s
    s

--- LEETCODE/LEETCODE/main.cpp
+++ LEETCODE/LEETCODE/main.cpp
@@ -5,13 +5,16 @@
 //  Created by 晏榆 on 2021/11/23.
 //
 /*
- 1、push_back是引用传递，[i]也是返回引用.
+ 1、
  2、定义定长的二维数组 int matrix[m][n]
  3、vector用法: v.erase(v.begin() + index) v.erase(v.end() - 1)
+    vector拷贝： vector<int> new_vector = old_vector;
+    push_back是引用传递，但其内部会复制一份；[i]也是返回引用.
  4、string用法: s.substr(start_index, length)
+ 
  5、sort用法: sort(v.begin(), v.end(), [](auto& a, auto& b){ return a > b})
- 6、vector拷贝： vector<int> new_vector = old_vector;
-
+ 6、map用法：map.find(key) == map().end() 表示找不到元素
+ 7、
  */
 #include <iostream>
 
