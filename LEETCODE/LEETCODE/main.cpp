3677e2363f128a91c344e59b5389b6bcd40ba0e3
Simon <simonyuyan@tencent.com>
2021-12-11 15:53:38 +0800

    ss

--- LEETCODE/LEETCODE/main.cpp
+++ LEETCODE/LEETCODE/main.cpp
@@ -10,6 +10,8 @@
  3、vector用法: v.erase(v.begin() + index) v.erase(v.end() - 1)
     vector拷贝： vector<int> new_vector = old_vector;
     push_back是引用传递，但其内部会复制一份；[i]也是返回引用.
+    vector<vector<int>>的初始化：
+ 
  4、string用法: s.substr(start_index, length)
  
  5、sort用法: sort(v.begin(), v.end(), [](auto& a, auto& b){ return a > b})
